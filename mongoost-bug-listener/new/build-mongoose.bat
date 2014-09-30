e06432d937e505fa1856f4173470481c2b44886a
Pavel Pimenov <pavel.pimenov@gmail.com>
2014-09-30 10:39:24 +0400

    * Remove

--- mongoost-bug-listener/new/build-mongoose.bat
+++
@@ -1,3 +0,0 @@
-del build-mongoose-bug.exe
-gcc -o build-mongoose-bug hello_world.c mongoose.c -W -Wall -Wno-unused-parameter -O0 -g -pipe -lwsock32
-build-mongoose-bug.exe
--- mongoost-bug-listener/old/build-mongoose.bat
+++
@@ -1,3 +0,0 @@
-del mongoose-server-ok.exe
-gcc -o mongoose-server-ok hello_world.c mongoose.c -W -Wall -Wno-unused-parameter -O0 -g -pipe -lwsock32 
-mongoose-server-ok.exe

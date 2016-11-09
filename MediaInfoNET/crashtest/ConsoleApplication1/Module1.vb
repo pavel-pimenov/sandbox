7a6cd05873655425b4ab3f393918f2b45434c0e0
pavel.pimenov <pavel.pimenov@gmail.com>
2016-11-09 08:54:39 +0300

    Add crashtest/ConsoleApplication1/ConsoleApplication1.vbproj

---
+++ MediaInfoNET/crashtest/ConsoleApplication1/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports MediaInfoNET
+
+Module Module1
+
+    Sub Main()
+        Dim aviFile As MediaFile = New MediaFile("D:\vm-share\mediainfo-crash\S09E15.mkv")
+
+        Console.WriteLine()
+        Console.WriteLine("General ---------------------------------")
+        Console.WriteLine()
+        Console.WriteLine("File Name   : {0}", aviFile.Name)
+        Console.WriteLine("Format      : {0}", aviFile.General.Format)
+        Console.WriteLine("Duration    : {0}", aviFile.General.DurationString)
+        Console.WriteLine("Bitrate     : {0}", aviFile.General.Bitrate)
+
+        If aviFile.Audio.Count > 0 Then
+            Console.WriteLine()
+            Console.WriteLine("Audio ---------------------------------")
+            Console.WriteLine()
+            Console.WriteLine("Format      : {0}", aviFile.Audio(0).Format)
+            Console.WriteLine("Bitrate     : {0}", aviFile.Audio(0).Bitrate.ToString())
+            Console.WriteLine("Channels    : {0}", aviFile.Audio(0).Channels.ToString())
+            Console.WriteLine("Sampling    : {0}", aviFile.Audio(0).SamplingRate.ToString())
+        End If
+
+        If aviFile.Video.Count > 0 Then
+            Console.WriteLine()
+            Console.WriteLine("Video ---------------------------------")
+            Console.WriteLine()
+            Console.WriteLine("Format      : {0}", aviFile.Video(0).Format)
+            Console.WriteLine("Bit rate    : {0}", aviFile.Video(0).Bitrate.ToString())
+            Console.WriteLine("Frame rate  : {0}", aviFile.Video(0).FrameRate.ToString())
+            Console.WriteLine("Frame size  : {0}", aviFile.Video(0).FrameSize.ToString())
+        End If
+
+        Console.ReadLine()
+    End Sub
+End Module
+
